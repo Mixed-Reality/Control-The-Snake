2768f9db66137635644e4879cdfa4e946478905e
Arjun <arj.kashyap123@gmail.com>
2021-04-28 23:25:20 +0530

    initialized a p5js sketch

---
+++ sketch.js
@@ -0,0 +1,13 @@
+const WIDTH = 400;
+const HEIGHT = 400;
+
+let MODEL_URL =
+  "https://cdn.jsdelivr.net/gh/ml5js/ml5-data-and-models/models/pitch-detection/crepe";
+
+function setup() {
+  createCanvas(WIDTH, HEIGHT);
+}
+
+function draw() {
+  background(0, 255, 0);
+}
